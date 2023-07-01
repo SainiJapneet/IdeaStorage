83bf1a5e06ed80643ea4dd059aa0392d2bcb00eb
japneet <japneetsaini084@gmail.com>
2023-07-01 20:25:53 +0530

    created basic API structure

--- Node_IdeaStorage/app.js
+++ Node_IdeaStorage/app.js
@@ -1,7 +1,22 @@
 const express = require('express')
-const dbConnect = require('./dbConnect')
+const dbConnect = require('./models/dbConnect')
 const Idea = require('./models/IdeaModel')
+const User = require('./models/userModel')
+const routes = require('./routes/routes');
 
 const app = express();
 dbConnect();
+
+//cors error
+app.use((req, res, next) => {
+    res.setHeader("Access-Control-Allow-Origin","*");
+    res.setHeader("Access-Control-Allow-Headers","Origin, X-Requested-With, Content, Content-Type, Authorization");
+    res.setHeader("Access-Control-Allow-Methods","POST, GET, PATCH, PUT, DELETE, OPTIONS");
+    next();
+})
+
+
+app.use('/auth',routes);
+app.use(express.urlencoded({ extended: true }));
+
 module.exports = app;
---
+++ Node_IdeaStorage/routes/routes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const userModel = require('../models/userModel');
+const ideaModel = require('../models/IdeaModel');
+const route = express.Router();
+
+
+
+module.exports = route;
